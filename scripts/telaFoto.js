eef6250ca2abce17b8c9ad6392091a1e50fd93af
Lina <anacarolina.siqueiramacedo@gmail.com>
2026-09-22 00:18:14 -0300

    analisar-imagem para analisar_imagem

--- scripts/telaFoto.js
+++ scripts/telaFoto.js
@@ -70,7 +70,7 @@ analisar.addEventListener("click", async function() {
     try {
 
         const resposta = await fetch(
-            "http://127.0.0.1:8000/analisar-imagem",
+            "http://192.168.68.105:8000/analisar_imagem",
             {
                 method: "POST",
                 body: dados
